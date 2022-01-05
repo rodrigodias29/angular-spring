f5bf6b4a3e4f6e079edd5d2061dfc940146b0f8f
rodrigodias29 <rodrigodiasreyes29@gmail.com>
2022-01-05 15:16:17 -0300

    aula 4

---
+++ crud-angular/src/app/courses/courses-routing.module.ts
@@ -0,0 +1,15 @@
+import { CoursesComponent } from './courses/courses.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'', component : CoursesComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CoursesRoutingModule { }
